f2adc364528c2736b748dabfc1f4d2128de616a9
Scott Fraser <scott@frasers.org>
2010-04-17 23:52:45 -0400

    modified code to not move path onto cell previously moved onto

--- src/org/frasers/quora/cooling/DatacenterCooling.clj
+++ src/org/frasers/quora/cooling/DatacenterCooling.clj
@@ -10,19 +10,22 @@
 (def datacenterinput '(2 0 0 0 0 0 0 0 0 0 3 1))
 
 
-(defn seek-end [datacentermap mypath x y]
+(defn seek-end [datacentermap x y]
   (let [paths (for [dx [(dec x) x (inc x)] dy [(dec y) y (inc y)]
                    :when (and
                             (not (and (= dx x) (= dy y)))
                             (not (neg? dx))
                             (not (neg? dy))
-                            (= 0 (datacentermap [dx dy])))]
-                [dx dy])]
+                            (= 0 (datacentermap [dx dy]))) ; only move to cells with a 0
+                   ]
+                [dx dy])]                           
     ; I AM HERE - this is getting the right paths - need to map these into agents
+    (prn datacentermap)
     (prn paths)
+
     ; Step 1 - see where we are... are we done?
     ; Step 2 - loop through all the paths and kick off a new agent for each one
-    ; Step 3 - would we just recur for one of the branches to avoid creating too many agents?
+    ; Step 3 - could we just recur for one of the branches to avoid creating too many agents?
     )
   )
 
@@ -40,7 +43,11 @@
 
     ; Now you have a map of [w h] to map entry, so for example the '2' is value for key [0 0]
     (prn datacentermap)
-    (seek-end datacentermap [[startx starty]] startx starty)
+    (seek-end
+      ; this assoc to "9" marks where we have walked
+      (assoc datacentermap [startx starty] 9)
+      ; this tells seek-end where we currently are
+      startx starty)
   ))
 
 (-main w h datacenterinput)
