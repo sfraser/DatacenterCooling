9a5f3fb8d8bb61d308864f0d8a8d91b7703a80a3
Scott Fraser <scott@frasers.org>
2010-04-24 20:06:17 -0400

    added some functions to calculate directions to move and simplify code

--- src/org/frasers/quora/cooling/DatacenterCooling.clj
+++ src/org/frasers/quora/cooling/DatacenterCooling.clj
@@ -2,20 +2,54 @@
   ; Parallelized solution to Quora Datacenter Cooling found at http://www.quora.com/challenges
   )
 
-(ns org.frasers.quora.cooling.DatacenterCooling
-  (:use clojure.contrib.duck-streams))
+(ns org.frasers.quora.cooling.DatacenterCooling)
+  ;(:use clojure.contrib.duck-streams))
 
 (def w 4)
 (def h 3)
 (def datacenterinput '(2 0 0 0 0 0 0 0 0 0 3 1))
 
+(defn above [x y]
+  ; I could probably remove the pos check or else to make this consistent check for overflow too
+  (if (pos? y)
+    [x (dec y)])
+  )
+
+(defn below [x y]
+  [x (inc y)]
+  )
+
+(defn left [x y]
+  (if (pos? x)
+  [(dec x) y])
+  )
+
+(defn right [x y]
+  [(inc x) y]
+  )
+
 
 (defn seek-end [datacentermap x y]
+  (let [paths (for [step [left right above below]
+                :when (= 0 (datacentermap (step x y)))]
+                (step x y))]
+    ; I AM HERE - this is getting the right paths - need to map these into agents
+    (prn datacentermap)
+    (prn paths)
+
+    ; Step 1 - see where we are... are we done?
+    ; Step 2 - loop through all the paths and kick off a new agent for each one
+    ; Step 3 - could we just recur for one of the branches to avoid creating too many agents?
+  ))
+
+
+(defn seek-end-old [datacentermap x y]
   (let [paths (for [dx [(dec x) x (inc x)] dy [(dec y) y (inc y)]
                    :when (and
                             (not (and (= dx x) (= dy y))) ; don't move onto the cell we already are on
                             (not (neg? dx)) ; don't move onto cells off the grid
                             (not (neg? dy)) ; ditto
+                            ; I AM HERE ELIMINATE DIAGONAL MOVEMENT
                             (= 0 (datacentermap [dx dy]))) ; only move to cells with a 0
                    ]
                 [dx dy])]                           
